48798c10a5ec4076229207fa6f1cb0c046a0b8e8
Torikul  Islam <96429071+torikul00@users.noreply.github.com>
2024-04-29 18:05:00 +0600

    footer completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,14 @@ import Navbar from "../components/Navbar";
 
 const Home = () => {
     return (
-        <header className="w-full h-screen bg-[#1A1A1A]">
+        <he>
             <HeaderContainer>
                 <Navbar />
                 <div>
                     <h1 className="text-3xl text-center">Home page Header here</h1>
                 </div>
             </HeaderContainer>
-        </header>
+        </he>
     );
 };
 
